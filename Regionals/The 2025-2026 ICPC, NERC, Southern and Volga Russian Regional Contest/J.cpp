b2e22ae9abaac5c814071f64ebcaf64c9c1cbbaa
Kadoz <97732441+K4doz@users.noreply.github.com>
2026-08-26 17:40:55 +0700

    NERC 2026

---
+++ Regionals/The 2025-2026 ICPC, NERC, Southern and Volga Russian Regional Contest/J.cpp
@@ -0,0 +1,103 @@
+/*
+    Author: Cadocx
+    Codeforces: https://codeforces.com/profile/Kadoc
+    VNOJ: oj.vnoi.info/user/Cadoc
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// input/output
+#define fastIO ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define el cout << '\n'
+#define debug(x) cout << #x << " = " << x << '\n'
+#define execute cerr << "Time elapsed: " << (1.0 * clock() / CLOCKS_PER_SEC) << "s"
+// #pragma GCC optimize("O2", "unroll-loops", "Ofast")
+// #pragma GCC target("avx,avx2,fma")
+//data type
+#define ll long long
+#define ull unsigned long long
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define piv pair<int, vector<int>>
+#define vi vector<int>
+#define vl vector<ll>
+#define vc vector<char>
+template<typename T> bool maximize(T &res, const T &val) { if (res < val){ res = val; return 1; }; return 0; }
+template<typename T> bool minimize(T &res, const T &val) { if (res > val){ res = val; return 1; }; return 0; }
+//STL
+#define sz(x) (int)(x).size()
+#define FOR(i,l,r) for(auto i = l; i <= r; i++)
+#define FORD(i,r,l) for(auto i = r; i >= l; i--)
+#define forin(i,a) for(auto i : a)
+#define pb push_back
+#define eb emplace_back
+#define pf push_front
+#define all(x) (x).begin(), (x).end()
+#define fi first
+#define se second
+//bitmask
+#define bitcnt(n) __builtin_popcount(n)
+#define MASK(i) (1 << (i))
+#define bit(n, i) (((n) >> (i)) & 1)
+#define set_on(n, i) ((n) | mask(i))
+#define set_off(n, i) ((n) & ~mask(i))
+//constant
+#define N 100005
+#define MOD 1000000007
+#define INF 0x3f3f3f3f
+#define LINF 0x3f3f3f3f3f3f3f3f
+#define base 31
+#define Kadoc 1
+
+int n;
+int pw[11];
+
+void solve(){
+    cin >> n;
+
+    // n -> len(n) luot xoay
+    // n -> cnt m = n + cnt + k * len
+
+    int cnt = 0;
+    int newn = n;
+    int len = log10(n) + 1;
+
+    set<int> Ans;
+    while(cnt < len){
+        ++cnt;
+
+        int bck = newn % 10;
+        newn /= 10;
+
+        newn = bck * pw[len-1] + newn;
+
+        if(newn - n - cnt <= 0) continue;
+        if((newn - n - cnt) % len == 0){
+            Ans.insert(newn - n);
+        }
+    }
+
+    cout << sz(Ans); el;
+    for(int x:Ans) cout << x << ' '; el;
+}
+
+int main(){
+    #define NAME "TASK"
+    if(fopen(NAME".inp", "r")){
+        freopen(NAME".inp", "r", stdin);
+        freopen(NAME".out", "w", stdout);
+    }
+
+    fastIO;
+
+    pw[0] = 1;
+    for(int i=1; i<=9; ++i) pw[i] = pw[i-1] * 10;
+    
+    if(Kadoc){
+        int tc; cin >> tc;
+        while(tc--){
+            solve();
+        }
+    } else solve();
+}
